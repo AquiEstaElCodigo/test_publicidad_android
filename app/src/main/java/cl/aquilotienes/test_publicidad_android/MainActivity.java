eb1dd5c09e960f056c091019ce3ab0cbcb5b0823
German-RS <german.riveross@gmail.com>
2020-05-22 19:23:01 -0400

    publicidad banner

---
+++ app/src/main/java/cl/aquilotienes/test_publicidad_android/MainActivity.java
@@ -0,0 +1,27 @@
+package cl.aquilotienes.test_publicidad_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+    }
+}
